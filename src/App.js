bed26a8691875130bd3ad52bdb5eda8c32ecd1c7
Nada Smith <ayronadas@gmail.com>
2023-08-01 09:39:54 -0400

    Updating Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      {/*components and routes*/}
+      <Routes>
+        <Route path='/' Component={Dashboard} />
+        <Route path='/patients' Component={PatientProfiles} />
+        <Route path='/prescriptions' Component={Prescriptions} />
+      </Routes>
+    </Router>
   );
 }
 
